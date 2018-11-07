f158b8f631569d52dded9878b08cad42cb89574a
Jens Andersson <jens.a@europeantravelventures.com>
2018-11-07 18:22:55 +0000

    Styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import "../styles.scss";
 
 export default () => (
-  <div>
-    <h1>Welcome</h1>
+  <div className="header">
+    <h1>Super Simple JSON CMS</h1>
+    <p className="lead">
+      Edit JSON online and access it through our API, a simple but powerful CMS
+    </p>
+    <img src="https://camo.githubusercontent.com/a1df47e8e574e0de60ce742891524c85edd09477/68747470733a2f2f7261772e6769746875622e636f6d2f6a6f7364656a6f6e672f6a736f6e656469746f722f6d61737465722f6d6973632f6a736f6e656469746f722e706e67" />
   </div>
-)
+);

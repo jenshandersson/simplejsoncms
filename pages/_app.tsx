8bad0bf1c56310bcbcd7e7f9af6054a2cd9f18a8
Jens Andersson <jens@fritan.com>
2024-05-13 15:36:19 +0200

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
 import "../styles.scss";
 import "../editor.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
